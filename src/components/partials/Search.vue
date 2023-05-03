8a82c1a002823be32df785f75b21a3f1526e2fed
andreaconv <andrea2convertini@gmail.com>
2023-05-03 18:58:46 +0200

    end

--- src/components/partials/Search.vue
+++ src/components/partials/Search.vue
@@ -16,7 +16,7 @@ export default {
     <option v-for="cardType in store.typeArray" :key="cardType" :value="cardType">{{ cardType }}</option>
   </select>
   
-  <button @click="$emit('searchCards')" >cerca</button>
+  <button @click="$emit('searchCards')" >Cerca</button>
 </template>
 
 <style lang="scss" scoped>
@@ -25,5 +25,12 @@ export default {
     padding: 5px;
     min-width: 150px;
     border: 0;
+  }
+
+  button{
+    border: 0;
+    border-radius: 5px;
+    padding: 5px 10px;
+    background-color: white;
   }
 </style>
